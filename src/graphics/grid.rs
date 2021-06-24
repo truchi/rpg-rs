a53b7e9ba0e4c1aafee71723e6f7ad4e08004c0e
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-06-24 10:03:22 +0200

    Subgrid shows from SCALE_LIMIT/2

--- src/graphics/grid.rs
+++ src/graphics/grid.rs
@@ -37,7 +37,16 @@ impl Grid {
 
     fn grid(mesh: &mut MeshBuilder, viewport: Viewport) {
         let green = Color::new(0., 1., 0., 0.5);
-        let red = Color::new(1., 0., 0., 0.25);
+        let red = Color::new(
+            1.,
+            0.,
+            0.,
+            if viewport.scale() >= SCALE_LIMIT / 2. {
+                0.25
+            } else {
+                0.
+            },
+        );
 
         let w = viewport.tile().x;
         let h = viewport.tile().y;
